9890b64d59ad7e14309b2c4f4621d1beb8954cd6
Max F. Gjacqual <89233376+gjacqual@users.noreply.github.com>
2021-12-21 19:21:42 +0300

    Update StrengthPasswordCheck.py

--- StrengthPasswordCheck.py
+++ StrengthPasswordCheck.py
@@ -1,25 +1,25 @@
-password = input("Введите пароль: ")
+password = input("Enter the password:")
 
 lenght = len(password)
 recomend = ''
 if lenght < 12:
-    recomend += f'увеличить число символов - {12 - lenght}, '
+    recomend += f'increase the number of characters - {12 - lenght}, '
 if not any(map(str.isupper, password)):
-    recomend += 'добавить 1 заглавную букву, '
+    recomend += 'add 1 capital letter, '
 if not any(map(str.islower, password)):
-    recomend += 'добавить 1 строчную букву, '
+    recomend += 'add 1 lowercase letter, '
 if not any(map(str.isdigit, password)):
-    recomend += 'добавить 1 цифру, '
+    recomend += 'add 1 digit, '
 isspec = any(simbol in '!@#$%^&*()-+' for simbol in password)
 if not isspec:
-    recomend += 'добавить 1 спецсимвол, '
+    recomend += 'add 1 special character, '
 if not password.isalnum() and not isspec:
-    print(f'Ошибка, Запрещенный символ')
+    print(f'Error, Forbidden character')
 else:
     if len(recomend) == 0:
-        print('Сильный пароль')
+        print('Strong password')
     else:
         fin_len = len(recomend)
-        print(f'Слабый пароль. Рекомендации: {recomend[: fin_len - 2]}.')
+        print(f'Weak password. Recommendations: {recomend[: fin_len - 2]}.')
 
 
